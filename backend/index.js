b44140860180d3cd4f846f4e419b2b3f915f0bfb
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-10-05 22:40:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const userPost = require("./controllers/Post/UserPost");
 const createPost = require("./controllers/Post/CreatePost");
 const cors  = require("cors");
 
-app.use(cors());
+// app.use(cors({
+//   origin: ["https://deploy-mern-1whq.vercel.app"], // Allow requests from this origin
+//   methods: ["POST", "GET"], // Allow only these HTTP methods
+//   credentials: true // Allow cookies and authorization headers to be included
+// }));
+
 require('dotenv').config();
 app.use(express.json());
 
