842a10ea70a1cce3062bccba2aeaa126d569992b
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-10-13 13:28:05 +0530

    Update Sigin.jsx

--- frontennd/src/pages/Sigin.jsx
+++ frontennd/src/pages/Sigin.jsx
@@ -6,6 +6,7 @@ import { BottomWarning } from "../components/BottomWarning";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import { NavBar } from "../components/AppBar";
 
 export function Signin({ setUser }) {
   const [error, SetError] = useState("");
@@ -22,7 +23,10 @@ export function Signin({ setUser }) {
     
     
   return (
+    <div>
+      <NavBar/>
     <div className="flex flex-col" style={{ height: 'calc(100vh - 76px)' }}>
+      
       <div className="flex flex-col items-center justify-center h-full bg-slate-300">
         <div className="rounded-lg bg-white w-80 text-center p-4">
           <Header label={"Sign in"} />
@@ -82,5 +86,6 @@ export function Signin({ setUser }) {
         </div>
       </div>
     </div>
+      </div>
   );
 }
